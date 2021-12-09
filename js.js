cc86ff3eaa70d4961d0d726079969f5d56076126
shivani718 <59058859+shivani718@users.noreply.github.com>
2021-12-09 22:28:35 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,15 @@
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EMCLD0J9SEPQ-2");
+// (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+//       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+//     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+//     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+//   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EMCLD0J9SEPQ-2");
+
+//segment
+!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="javWjpX2qUqY94GDjEB4FSXB8Y1gd6RK";;analytics.SNIPPET_VERSION="4.15.3";
+  analytics.load("javWjpX2qUqY94GDjEB4FSXB8Y1gd6RK");
+  analytics.page();
+  }}();
+
 
 function login()
     {
